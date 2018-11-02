a33ba7c5fa84c04be2c4e69016e64cc97228e738
Greg Morgan <gregm9999@gmail.com>
2018-11-02 00:46:23 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,13 +2,9 @@ def using_until
   levitation_force = 6
   #your code here
     loop do
-<<<<<<< HEAD
       puts "Wingardium Leviosa"
-=======
->>>>>>> 247797c14f229f588e427fbf2ac8e0b2fc9b23e0
       levitation_force+=1
       if levitation_force>=10
         break
       end
-end
 end
